52d1ed730a5e379bdfb0cb447099a8ac59569758
Jonathan Hsu <jonhsu.design@gmail.com>
2021-03-25 15:07:33 -0700

    finished component library

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,25 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    height: 24 + 'px',
+    '--width': 150 + 'px',
+    '--border': '1px solid black',
+    '--fontSize': 14 / 16 + 'rem',
+    iconSize: 16,
+    '--padding': 24 + 'px',
+  }, 
+  large: {
+    height: 36 + 'px',
+    '--width': 300 + 'px',
+    '--border': '2px solid black',
+    '--fontSize': 18 / 16 + 'rem',
+    iconSize: 24,
+    '--padding': 36 + 'px',
+  },
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +32,50 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = SIZES[size];
+
+  return (
+    <Wrapper style={styles}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+      <Input placeholder={placeholder} />
+    </Wrapper>
+  )
 };
+
+const Wrapper = styled.div`
+  display: block;
+  position: relative;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`
+
+const Input = styled.input`
+  width: var(--width);
+  height: var(--height);
+  border: none;
+  border-bottom: var(--border);
+  font-size: var(--fontSize);
+  padding-left: var(--padding);
+  outline-offset: 2px;
+  font-weight: 700;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,59 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--height': 8 + 'px',
+    '--borderRadius': 4 + 'px',
+    '--padding': 0 + 'px',
+  },
+  medium: {
+    '--height': 12 + 'px',
+    '--borderRadius': 4 + 'px',
+    '--padding': 0 + 'px',
+  },
+  large: {
+    '--height': 24 + 'px',
+    '--borderRadius': 8 + 'px',
+    '--padding': 4 + 'px',
+  },
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+  
+  return (
+    <ProgressWrapper
+      role='progressbar'
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={styles}
+    >
+      <Progress value={value} />
+    </ProgressWrapper>
+  )
 };
 
 export default ProgressBar;
+
+const ProgressWrapper = styled.div`
+  height: var(--height);
+  border-radius: var(--borderRadius); 
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+`
+
+const Progress = styled.div`
+  background: ${COLORS.primary};
+  border-radius: 4px;
+  width: ${props => (props.value / 1) + '%'};
+  height: 100%;
+
+
+  ${({value}) => value < 100 && `
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  `}
+`
